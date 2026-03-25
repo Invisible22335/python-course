1aa9571f8c1b9f04ea9d8c938ecf4756ac347c67
Invisible22335 <164178919+Invisible22335@users.noreply.github.com>
2026-03-25 23:46:10 +0500

    Delete practice/mod1 directory

--- practice/mod1/task3.py
+++
@@ -1,6 +0,0 @@
-numbers = list(map(int, input("Введите числа через пробел: ").split()))
-k = int(input("Введите число K: "))
-
-result = [x for x in numbers if x % k == 0]
-
-print("Числа, кратные K:", result)
--- practice/mod1/task4.py
+++
@@ -1,5 +0,0 @@
-phrases = input("Введите фразы через ; : ").split(";")
-lengths = [len(phrase.split()) for phrase in phrases]
-
-print("Исходный список:", phrases)
-print("Список lengths:", lengths)
